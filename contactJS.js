72e36e8e852c73c4b16da76dc8452e5f14a4642e
Michael Li <dragontt16@gmail.com>
2019-01-18 21:04:12 -0500

    contact form

---
+++ contactJS.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	$(".submit").click(function(event){
+		
+		var email = $(".email").val();
+		var subject = $(".subject").val();
+		var message = $(".message").val();
+		var statusElm = $(".status")
+		statusElm.empty();
+		
+		if(!(email.includes("@") && email.includes("."))){
+			event.preventDefault();
+			statusElm.append("<div>Email invalid</div>");
+		}
+		
+		if(subject.length < 1){
+			var conf = confirm("Send email with no subject?");
+			if(!conf){
+				event.preventDefault();
+			}
+		}
+		
+		if(message.length < 1){
+			event.preventDefault();
+			statusElm.append("<div>Message is empty<div>");
+		}
+	})
+})
